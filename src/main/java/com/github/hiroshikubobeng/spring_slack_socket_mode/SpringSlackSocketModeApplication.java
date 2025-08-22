a33bd4426c4472684ed9c4589985182ea1186076
kubo-h <kubo-h@to-be.co.jp>
2025-08-22 20:39:16 +0900

    first commit ( spring initializr)

---
+++ src/main/java/com/github/hiroshikubobeng/spring_slack_socket_mode/SpringSlackSocketModeApplication.java
@@ -0,0 +1,13 @@
+package com.github.hiroshikubobeng.spring_slack_socket_mode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSlackSocketModeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSlackSocketModeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/hiroshikubobeng/spring_slack_socket_mode/SpringSlackSocketModeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hiroshikubobeng.spring_slack_socket_mode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSlackSocketModeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
